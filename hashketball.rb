14a4beb26932355c4fba039c08d8fd5e18546650
John Reiner <johnreiner1990@gmail.com>
2020-01-17 01:48:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -241,7 +241,7 @@ def big_shoe_rebounds
       if team_keys == :players
         team_values.each do |player|
           player.each do |player_key, player_value|
-            # binding.pry
+            binding.pry
             if player_value[:shoe] == 19
               return player_key[:rebounds]
             end
